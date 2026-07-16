2e3e4bdd69fcf7e7b6e3570d47a5c328b959e46b
agg-kantas <agg.kantas@gmail.com>
2026-07-16 22:47:45 +0300

    Changed the scanner cooldown

--- pico/main.py
+++ pico/main.py
@@ -2,7 +2,6 @@ from mfrc522 import MFRC522
 import time
 reader = MFRC522(sck=2,mosi=3,miso=4,rst=0,cs=1)
 reader.init()
-print("Reader Initialized"+"\n")
 while True:
     stat,bits = reader.request(reader.REQIDL)
     if stat==reader.OK: # checks if its picking up a card
@@ -11,5 +10,4 @@ while True:
             uid_bytes = bytes(uid) #converts uid to bytes
             card = int.from_bytes(uid_bytes,"little",False) #converts bytes in little Endian order, unassigned, to an integer
             print(card)
-            time.sleep(1)
-
+            time.sleep(0.5)
