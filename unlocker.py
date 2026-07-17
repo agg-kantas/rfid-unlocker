1c5cde51ab78dfe71b14af3adda6d223ce75107d
agg-kantas <agg.kantas@gmail.com>
2026-07-17 22:59:26 +0300

    Fixed some bugs and added exception errors

--- unlocker.py
+++ unlocker.py
@@ -5,29 +5,35 @@ class Unlocker:
     def __init__(self):
         self.card = 299630633 #configure this according to the uid you want to be accepted
         self.port = serial.Serial("/dev/ttyACM0",1000000) #configure this according to your microcontroller directory and its baudrate
+        self.acc = None
+        self.session = 3 #configure this with your own session ID
     def compare(self):
         print(f"Connected to {self.port.name}")
         while True:
             line = self.port.readline() #reads line from port
             line = line.decode().strip() #leaves the pure string
             line = int(line)
-            if line == self.card:
-                self.acc = True
-                break
+            try:
+                if line == self.card:
+                    self.acc = True
+                    break
+            except ValueError:
+                    print("Error! Microcontroller picked up malformed data!")
             else:
                 self.acc = False
                 break
-        self.port.close()
     def unlock(self):
         if self.acc == True:
             print("Card was recognized successfully!")
-            process = subprocess.Popen(["loginctl unlock-session 3"],shell=True) #configure this with your own user session ID
-        else:
+            process = subprocess.Popen([f"loginctl unlock-session {self.session}"],shell=True)
+        elif self.acc == False:
             print("Card was not recognized")
+        else:
+            print("Error! No card detected")
 if __name__ == "__main__": #checks if program is ran as file instead of being imported
     proc = Unlocker()
     while True:
-        proc = Unlocker()
-        time.sleep(1)
         proc.compare()
+        time.sleep(1)
         proc.unlock()
+    self.port.close()
